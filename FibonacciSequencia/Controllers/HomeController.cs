025b88047ea607ecef6705bfa36254804da77ba7
Henrique Valvassori <henriquevlvassori41@gmail.com>
2024-09-19 16:39:34 -0300

    first commit

---
+++ FibonacciSequencia/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using FibonacciSequencia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FibonacciSequencia.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(FibonacciModel numero)
+        {
+            ViewBag.Message = IsFibonacci(numero.Numero) ? "Pertence" : "Não Pertence";
+            return View();
+        }
+
+        private bool IsFibonacci(int c)
+        {
+            int a = 0, b = 1, i;
+
+            for (i = a = b; i <= c; i = a + b)
+            {
+                a = b;
+                b = i;
+            }
+
+            return i == c;
+        }
+
+
+
+    }
+
+       
+
+       
+    
+}
---
+++ FibonacciSequencia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FibonacciSequencia.MvcApplication" Language="C#" %>
